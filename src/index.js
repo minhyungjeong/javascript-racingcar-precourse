92e43b6ba10d35d6740c832f7a76420c48658d15
minbr0ther <minbr0ther@naver.com>
2021-11-22 21:30:04 +0900

    feat: 초기화면 세팅

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 export default function RacingCarGame() {
-  const addIdTag = () => {
-    const carNamesInput = (document.querySelectorAll('input')[0].id =
-      'car-names-input');
-    const carNamesSubmit = (document.querySelectorAll('button')[0].id =
-      'car-names-submit');
-    const racingCountInput = (document.querySelectorAll('input')[1].id =
-      'racing-count-input');
-    const racingCountSubmit = (document.querySelectorAll('button')[1].id =
-      'racing-count-submit');
-  };
+  document.querySelectorAll('form')[0].id = 'car-names-form';
+  document.querySelectorAll('input')[0].id = 'car-names-input';
+  document.querySelectorAll('button')[0].id = 'car-names-submit';
+  document.querySelectorAll('h4')[0].id = 'racing-count-title';
+  document.querySelectorAll('form')[1].id = 'racing-count-form';
+  document.querySelectorAll('input')[1].id = 'racing-count-input';
+  document.querySelectorAll('button')[1].id = 'racing-count-submit';
+  document.querySelectorAll('h4')[1].id = 'racing-result';
 
-  addIdTag();
+  const carNamesFrom = document.getElementById('car-names-form');
+  const carNamesInput = document.getElementById('car-names-input');
+  const racingCountResult = document.getElementById('racing-count-title');
+  const racingCountForm = document.getElementById('racing-count-form');
+  const racingResult = document.getElementById('racing-result');
+
+  racingCountResult.style.display = 'none';
+  racingCountForm.style.display = 'none';
+  racingResult.style.display = 'none';
+
+  carNamesFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(carNamesInput.value);
+  });
 }
 
 new RacingCarGame();
