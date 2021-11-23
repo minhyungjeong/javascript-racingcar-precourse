f49d9f68a0c473ee27290d30197aed9a61af28b1
minbr0ther <minbr0ther@naver.com>
2021-11-23 16:07:24 +0900

    feat: Car 객체 생성

--- src/isValidCarNames.js
+++ src/isValidCarNames.js
@@ -6,12 +6,14 @@ import {
   CAR_NAMES_DUPLICATED,
 } from './constants.js';
 
-const isValidCarNames = (string, form) => {
-  if (!/^[a-z]|^[\,]/.test(string)) {
+import Car from './Car.js';
+
+const isValidCarNames = (names, form) => {
+  if (!/^[a-z]|^[\,]/.test(names)) {
     alert(REQUIRE_ATOZ_AND_COMMA);
     form.reset();
   }
-  const carNamesArr = string.split(CAR_NAMES_SEPERATOR);
+  const carNamesArr = names.split(CAR_NAMES_SEPERATOR);
   const duplicateCheck = new Set(carNamesArr);
 
   if (carNamesArr.length !== duplicateCheck.size) {
@@ -19,18 +21,22 @@ const isValidCarNames = (string, form) => {
     form.reset();
   }
 
-  carNamesArr.forEach((str) => {
-    if (/\s/g.test(str) || str === '') {
+  let cars = [];
+  carNamesArr.forEach((name) => {
+    if (/\s/g.test(name) || name === '') {
       alert(INCLUDING_SPACE);
       form.reset();
       return;
     }
-    if (str.length > 5) {
+    if (name.length > 5) {
       alert(REQUIRE_UNDER_FIVE_LETTER);
       form.reset();
       return;
     }
+    cars.push(new Car(name));
   });
+
+  cars.forEach((obj) => console.log(obj.drivingDistance));
 };
 
 export default isValidCarNames;
