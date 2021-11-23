0905855c76554200c46a07a15e05bd65436ee495
minbr0ther <minbr0ther@naver.com>
2021-11-23 11:38:24 +0900

    feat: 공백 입력, 특수문자 사용, 영어 소문자 이외의 입력 방지

--- src/isValidCarNames.js
+++ src/isValidCarNames.js
@@ -1,3 +1,8 @@
 export const isValidCarNames = (string) => {
-  console.log(string);
+  //a-z으로 이루어진 영어가 포함되어 있거나 ,가 포함되어있는 경우
+  if (/\s/g.test(string)) {
+    alert('공백이 포함되어 있습니다.');
+  } else if (!/^[a-z]|^[\,]/.test(string)) {
+    alert('a-z(영어 소문자)로 이루어진 이름과 콤마로 구분하여 입력해주세요!');
+  }
 };
