db451eab0eaf7fa646b924ef01b35d1f588e76ca
minbr0ther <minbr0ther@naver.com>
2021-11-23 14:19:42 +0900

    refactor: 엘리먼트들에 id값을 부여하는 부분 분리

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import { isValidCarNames } from './isValidCarNames.js';
+import isValidCarNames from './isValidCarNames.js';
+import elements from './elements.js';
 
 export default function RacingCarGame() {
-  document.querySelectorAll('form')[0].id = 'car-names-form';
-  document.querySelectorAll('input')[0].id = 'car-names-input';
-  document.querySelectorAll('button')[0].id = 'car-names-submit';
-  document.querySelectorAll('h4')[0].id = 'racing-count-title';
-  document.querySelectorAll('form')[1].id = 'racing-count-form';
-  document.querySelectorAll('input')[1].id = 'racing-count-input';
-  document.querySelectorAll('button')[1].id = 'racing-count-submit';
-  document.querySelectorAll('h4')[1].id = 'racing-result';
+  elements.addElementId();
 
-  const carNamesFrom = document.getElementById('car-names-form');
+  const carNamesForm = document.getElementById('car-names-form');
   const carNamesInput = document.getElementById('car-names-input');
   const racingCountResult = document.getElementById('racing-count-title');
   const racingCountForm = document.getElementById('racing-count-form');
@@ -20,9 +14,9 @@ export default function RacingCarGame() {
   racingCountForm.style.display = 'none';
   racingResult.style.display = 'none';
 
-  carNamesFrom.addEventListener('submit', (e) => {
+  carNamesForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    isValidCarNames(carNamesInput.value);
+    isValidCarNames(carNamesInput.value, carNamesForm);
   });
 }
 
