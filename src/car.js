a7c7f2f1d197fc3818f2174e2878f01fa051c72f
minbr0ther <minbr0ther@naver.com>
2021-11-23 20:02:10 +0900

    feat: 실행 결과 보여주기

--- src/car.js
+++ src/car.js
@@ -2,13 +2,13 @@ import { MIN_NUMBER, MAX_NUMBER } from './constants.js';
 
 export default function Car(name) {
   this.name = name;
-  this.numberOfdrivingDistance = MIN_NUMBER;
+  this.numberOfDrivingDistance = MIN_NUMBER;
   this.hypenOfdrivingDistance = '';
 
   this.tryGoForward = () => {
     const randZeroToNine = Math.floor(Math.random() * MAX_NUMBER);
     if (randZeroToNine >= 4) {
-      this.numberOfdrivingDistance++;
+      this.numberOfDrivingDistance++;
       this.hypenOfdrivingDistance += '-';
     }
   };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import isValidCarNames from './isValidCarNames.js';
 import elements from './elements.js';
+import { CAR_NAMES_SEPERATOR } from './constants.js';
 
 export default function RacingCarGame() {
   elements.addElementId();
 
   // console.log(elements.carNamesForm); //이렇게 하면 Null 뜸,,
 
+  const app = document.getElementById('app');
   const carNamesForm = document.getElementById('car-names-form');
   const carNamesInput = document.getElementById('car-names-input');
   const carNamesSubmit = document.getElementById('car-names-submit');
@@ -41,15 +43,31 @@ export default function RacingCarGame() {
 
   const playGame = (numberOfPlay) => {
     for (let i = 0; i < numberOfPlay; i++) {
-      cars.forEach((car) => {
-        car.tryGoForward();
-        racingResult.insertAdjacentHTML(
-          'afterend',
-          `<div>${car.name}: ${car.hypenOfdrivingDistance}<div>`
-        );
-      });
-      racingResult.insertAdjacentHTML('afterend', `<br>`);
+      tryMove(cars);
     }
+    showResult();
+  };
+
+  const tryMove = (cars) => {
+    cars.forEach((car) => {
+      car.tryGoForward();
+      app.insertAdjacentHTML(
+        'beforeend',
+        `<div>${car.name}: ${car.hypenOfdrivingDistance}</div>`
+      );
+    });
+    app.insertAdjacentHTML('beforeend', `<br>`);
+  };
+
+  const showResult = () => {
+    let maxDistance = 0;
+    cars.forEach((car) => {
+      maxDistance = Math.max(maxDistance, car.numberOfDrivingDistance);
+    });
+
+    cars = cars.filter((car) => car.numberOfDrivingDistance === maxDistance);
+    const winners = cars.map((car) => car.name).join(CAR_NAMES_SEPERATOR);
+    app.insertAdjacentHTML('beforeend', `<h4>최종 우승자: ${winners}</h4>`);
   };
 }
 
